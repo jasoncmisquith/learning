5d7867ebb360495b23e9affd66d3adfb5d48d601
jasoncmisquith <jsnmisquith2@gmail.com>
2023-04-09 21:58:53 +0530

    nest updates

--- nestjs/learnnest/src/app.module.ts
+++ nestjs/learnnest/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
-@Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+@Module({ 
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
--- nestjs/learnnest/src/auth/auth.service.ts
+++ nestjs/learnnest/src/auth/auth.service.ts
@@ -1,12 +1,40 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, ForbiddenException } from "@nestjs/common";
+import { User, Bookmark } from "@prisma/client"
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
     signin() {
         return {msg: 'I have signedin'}
     }
 
-    signup() {
-        return {msg: 'I have signedup'}
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            });
+            delete user.hash;
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials Taken',)
+                }
+            }
+            throw error;
+        }
+
+
     }
 }
